1083eb3515bbe7d2ff78b655a2c72136675ed2b6
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-09-24 18:37:25 -0700

    final updates to html, script, css made

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,7 @@
-// API key 17491cca6b7cf053d78af447ad4c8844
+/* Variables */
 
 var APIKey = "17491cca6b7cf053d78af447ad4c8844";
-
-
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
 var cityButtons = document.querySelector('#cityButton');
-// var cityNames = document.querySelector('cityName');
 
 
 var city ;
@@ -25,22 +20,13 @@ var clearSearch = document.querySelector('#clearButton');
 
 var cityInput = function (event) {
     event.preventDefault();
-
     city = cityTypeSearch.value.trim();
-    console.log(city);
-    // addSearch(city);
     weatherAPI(event);
+    submissionCheck(city);
+
 }
 
-
-
-
-
 var buttonClick = function(event) {
-    // var city = event.target.getAttribute
-
-        
-
 
     city = event.target.getAttribute
     ('data-language');
@@ -51,11 +37,7 @@ var buttonClick = function(event) {
 }
 
 
-// Storage for searched cities in local storage
-
 var searchList = [];
-
-// cityButtonSearch add in another eventlistener to add in to array
 
 function addSearch(event) {
 searchList.push(event);
@@ -63,30 +45,6 @@ searchList.push(event);
 localStorage.setItem("cities",JSON.stringify(searchList))
 loadSearch();
 }
-
-// function storeSearch () {
-// localStorage.setItem("cities",JSON.stringify(searchList))
-// }
-
-
-// grab stored data and reload it, need to have this happen on the start of the page, and also when an additional search is added
-
-// function pageLoad() {
-//     searchList = JSON.parse(localStorage.getItem("cities"));
-  
-//     $('#cityButton').empty();
-
-//     $('#cityButton').append(
-//         $(document.createElement('button')).prop({
-//             type: 'button',
-//             innerHTML: cityName,
-//             'data-language':cityName,
-//             class: 'btn btn-primary m-2 btn-lg btn-block text-center',
-          
-//         }).attr('data-language',cityName)
-        
-// )
-// }
 
 
 
@@ -99,9 +57,6 @@ function loadSearch() {
 
 
 for (i=0; i<searchList.length; i++) {
-
-// grab an item from the array
-// then take this item value, create a button with the class id, and then append this button to the list
 
 var cityName = searchList[i];
 
@@ -130,38 +85,43 @@ function clearCities(event) {
 
 
 
+function submissionCheck(event) {
+    
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?&q=" + city + "&cnt=6&appid=" + APIKey;
+
+    fetch(queryURL)
+    .then(function(response){
+        if(response.ok)
+        addSearch(event);
+    })
+}
+
+
+
 function weatherAPI(event) {
 
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?&q=" + city + "&cnt=6&appid=" + APIKey;
 
 fetch(queryURL)
-
 .then(function (response) {
     if(response.ok) {
     return response.json().then(function(data) {
-        addSearch(city);
+
         $('#data').text(data);
-        console.log(data);
         cityName = data.name
         var currentTemperature = (((1.8)*((data.main.temp)-273.15))+32).toFixed(2);
         var currentWeatherIcon = " http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-
-       
         $('#currentDateIcon').attr('src',currentWeatherIcon);
         $('#cityName').text(data.name);
         $('#currentTemperature').text("Temp: " + currentTemperature);
         $('#currentWind').text("Wind Speed: " +data.wind.speed);
         $('#currentHumidity').text("Humidity: " +data.main.humidity);
-        console.log(currentDate);
         var lat = data.coord.lat;
         var lon = data.coord.lon;
-        console.log(lat);
-        console.log(lon);
        
         weatherData(lat,lon);
+
     })
-
-
 
 }
 
@@ -170,38 +130,13 @@ fetch(queryURL)
         return;
     }
 
-
-
-
 }
 )
     .catch(function(error) {
         alert('unable to connect to API');
     });
-
-// .then(function(data) {
-//     $('#data').text(data);
-//     console.log(data);
-//     // console.log(data.weather[0].description);
-//     cityName = data.name
-//     console.log(cityName);
-
-//     var currentTemperature = (((1.8)*((data.main.temp)-273.15))+32)
-
-//     $('#cityName').text(data.name);
-//     $('#currentTemperature').text(currentTemperature);
-//     $('#currentWind').text(data.wind.speed + " wind speed");
-//     $('#currentHumidity').text(data.main.humidity + " humidity");
-//     console.log(currentDate);
-//     var lat = data.coord.lat;
-//     var lon = data.coord.lon;
-//     console.log(lat);
-//     console.log(lon);
-   
-//     weatherData(lat,lon);
-// })
-
 }
+
 
 function weatherData(la, lo)  {
 var  APIKey = "17491cca6b7cf053d78af447ad4c8844";
@@ -209,10 +144,6 @@ var  APIKey = "17491cca6b7cf053d78af447ad4c8844";
 var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + la + "&lon=" + lo + "&appid=" + APIKey;
 
 
-// var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=${la}&lon=${lo}&exclude=minutely,hourly&units=imperial&appid=${APIKey}";
-
-
-// var weatherURL ="https://api.openweathermap.org/data/2.5/onecall?lat=${la}&lon=${lo}&exclude=minutely,hourly&units=imperial&appid=${APIKey}";
 fetch(weatherURL) 
     .then(function (response) {
         return response.json();
@@ -227,24 +158,10 @@ fetch(weatherURL)
         console.log(data.list[0].main.temp)
 
 
-        //initial set for the current day
-        // data.list[i].clouds.dt_txt;
-        // data.list[i].weather[0].icon;
-        // data.list[i].main.temp;
-        // data.list[i].wind.speed;
-        // data.list[i].main.humidity;
+    
 
-
-        //this will be for the smaller boxes
         for (i=1;i<6;i++) {
-            // create an element of some kind that will create a box
-            //date
-            //icon
-            //temp
-            //wind
-            //humity
-
-            http://openweathermap.org/img/wn/10d@2x.png
+    
 
             var weatherIcon = " http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
 
@@ -305,23 +222,7 @@ fetch(weatherURL)
                     console.log(data.list[j].dt_txt);
                  
                 }
-
-
- //display these, and then append again to create a row of boxes
-        //     console.log(data.list[i].clouds.dt);
-        //     console.log(data.list[i].weather[0].icon);
-        //     console.log(data.list[i].main.temp);
-        //     console.log(data.list[i].wind.speed);
-        //     console.log(data.list[i].main.humidity);
-        //     console.log(weatherIcon);
-
-        //    console.log(timeNow);
-
-
         }
-
-
-
 
     } 
     })
@@ -330,7 +231,7 @@ fetch(weatherURL)
 loadSearch();
 cityButtons.addEventListener('click', buttonClick);
 cityTypeSearchButton.addEventListener('submit', cityInput);
-// cityTypeSearchButton.addEventListener('submit,', addSearch);
+// cityTypeSearchButton.addEventListener('submit',submissionCheck());
 clearSearch.addEventListener('click', clearCities);
 
 
